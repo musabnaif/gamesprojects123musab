2467d41922ff6f22ad3fd0445c335337929aa594
musabnaif <75007703+musabnaif@users.noreply.github.com>
2020-11-25 10:23:13 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzY3NTc1OTk3NDg2MzMzOTYz.X4z62g.OhV9a2DEbCtTF8TMvwWCYt1VL-I", /// حط التوكن هنا
+  YT_API_KEY: "api ",
+  prefix: "-",
+  devs: ["616709497817268281"] ///  حط ايديك هنا
+};
